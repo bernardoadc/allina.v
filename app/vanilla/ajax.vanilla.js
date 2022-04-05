de4b61c46769ce1ddc94de3343a8a9b502f959ef
Bernardo A. Dal Corno <bernardoadc@gmail.com>
2022-04-05 08:06:28 -0400

    ajax with json5

--- app/vanilla/ajax.vanilla.js
+++ app/vanilla/ajax.vanilla.js
@@ -1,8 +1,11 @@
-const Ajax = {
+import json5 from '../../json5/dist/index.min.mjs'
+
+const ajax = {
   get: (url) => fetch(url, { method: 'GET' }).then(r => r.text()),
   post: (url, data) => fetch(url, { method: 'POST', data }).then(r => r.json()),
 
-  getJSON: (url) => fetch(url, { method: 'GET' }).then(r => r.json())
+  getJSON: (url) => fetch(url, { method: 'GET' }).then(r => r.json()),
+  getJSON5: (url) => fetch(url, { method: 'GET' }).then(r => r.text()).then(r => json5.parse(r))
 }
 
-export default Ajax
+export default ajax
